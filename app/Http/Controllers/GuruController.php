da5bf1ec1290dc3e90351bb98bc51d543f163cc5
azmal99 <rizkyazmal99@gmail.com>
2022-06-12 00:22:37 +0700

    fixing error handling

--- app/Http/Controllers/GuruController.php
+++ app/Http/Controllers/GuruController.php
@@ -48,16 +48,17 @@ class GuruController extends BaseController
     //CREATE
     public function store(Request $request)
     {
-        try{
-            $guru = new Guru();
-            $randomId = rand(1,99);
-            $guru->id = $randomId;
-            $guru->kd_guru = ($request->input('kd_guru'));
-            $guru->nama_guru = ($request->input('nama_guru'));
-            $guru->enable_flag = ($request->input('enable_flag'));
-            
-            $guru->save();
-            
+        $guru = new Guru();
+        $randomId = rand(1,99);
+        $guru->id = $randomId;
+        $guru->kd_guru = ($request->input('kd_guru'));
+        $guru->nama_guru = ($request->input('nama_guru'));
+        $guru->enable_flag = ($request->input('enable_flag'));
+        
+        $guru->save();
+
+        $code = $e->getStatusCode();
+        if ($code =! 500){
             return response()->json([
                 'success' => true,
                 'message' => 'Berhasil Tambah Guru',
@@ -66,18 +67,44 @@ class GuruController extends BaseController
                     ],
             ], 201)
             ->header('Access-Control-Allow-Origin', '*');
-        }catch (Throwable $e){
-            report($e);
-
+        }else {
             return response()->json([
                 'success' => false,
-                'message' => 'Gagal Menambahkan Guru',
-                'data' =>[
-                    'user' => $guru,
-                    ],
+                'message' => 'Gagal Tambah Guru',
             ], 500)
             ->header('Access-Control-Allow-Origin', '*');
         }
+        
+        // try{
+        //     $guru = new Guru();
+        //     $randomId = rand(1,99);
+        //     $guru->id = $randomId;
+        //     $guru->kd_guru = ($request->input('kd_guru'));
+        //     $guru->nama_guru = ($request->input('nama_guru'));
+        //     $guru->enable_flag = ($request->input('enable_flag'));
+            
+        //     $guru->save();
+            
+        //     return response()->json([
+        //         'success' => true,
+        //         'message' => 'Berhasil Tambah Guru',
+        //         'data' =>[
+        //             'user' => $guru,
+        //             ],
+        //     ], 201)
+        //     ->header('Access-Control-Allow-Origin', '*');
+        // }catch (Throwable $e){
+        //     report($e);
+
+        //     return response()->json([
+        //         'success' => false,
+        //         'message' => 'Gagal Menambahkan Guru',
+        //         'data' =>[
+        //             'user' => $guru,
+        //             ],
+        //     ], 500)
+        //     ->header('Access-Control-Allow-Origin', '*');
+        // }
         
     }
 
