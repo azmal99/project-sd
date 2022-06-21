12194b69aaf5fe3914a1c55167c39ed1078ea1f8
azmal99 <rizkyazmal99@gmail.com>
2022-06-21 22:46:03 +0700

    fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -75,7 +75,7 @@ class AuthController extends BaseController
                 $apiToken = base64_encode(Str::random(32));
 
                 $guru->api_token = $apiToken;
-                $guri->save();
+                $guru->save();
 
                 return response()->json([
                     'success' => true,
