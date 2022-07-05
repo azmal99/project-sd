8c8e957f8db237d016505fb570acdbea0a88198f
azmal99 <rizkyazmal99@gmail.com>
2022-07-05 21:24:07 +0700

    commit

--- app/Http/Controllers/MapelController.php
+++ app/Http/Controllers/MapelController.php
@@ -33,8 +33,8 @@ class MapelController extends BaseController
     {
         // $mata_pelajaran = MataPelajaran::where('guru_id', $guru_id)->first();
 
-        $mata_pelajaran = DB::table('mata_pelajaran mp')
-        ->select( 'mp.kd_mata_pelajaran' , 'mp.nama_mata_pelajaran', 'guru.nama_guru')
+        $mata_pelajaran = DB::table('mata_pelajaran')
+        ->select( 'mata_pelajaran.kd_mata_pelajaran' , 'mata_pelajaran.nama_mata_pelajaran', 'guru.nama_guru')
         ->join('mata_pelajaran','mata_pelajaran.guru_id','=','guru.id')
         ->get();
 
@@ -78,10 +78,9 @@ class MapelController extends BaseController
     public function showByGuruId($guru_id)
     {
         // $mata_pelajaran = MataPelajaran::where('guru_id', $guru_id)->first();
-
         $mata_pelajaran = DB::table('mata_pelajaran')
         ->select( 'mata_pelajaran.kd_mata_pelajaran' , 'mata_pelajaran.nama_mata_pelajaran', 'guru.nama_guru')
-        ->where('guru.id', '=', $guru_id)
+        ->where('mata_pelajaran.guru_id', $guru_id)
         ->join('mata_pelajaran','mata_pelajaran.guru_id','=','guru.id')
         ->get();
 
