a1052ffe800909745969d96abc3e7693b2bf68e7
azmal99 <rizkyazmal99@gmail.com>
2022-07-09 10:32:58 +0700

    fixing

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -156,9 +156,9 @@ class SiswaController extends BaseController
         }
 
         $kelas_id = $siswa->kelas_id;
-        $kelas_siswa = DB::select('select kd_kelas from kelas where id = ' + $kelas_id);
+        $kelas_siswa = DB::select('select kd_kelas from kelas where id = ?' [$kelas_id]);
         $kelas_ob = (object) $kelas_siswa;
-        $mapel_siswa = DB::select('select id from mata_pelajaran where kd_mata_pelajaran like ' + $kelas_siswa + '%');
+        $mapel_siswa = DB::select('select id from mata_pelajaran where kd_mata_pelajaran like ?%' [$kelas_siswa]);
         $mapel_size = count($mapel_siswa);
 
         for($i=0; $i<=$mapel_size; $i++){
