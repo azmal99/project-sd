ef1f99f39d3b9fd583f4a77e8f2858266b1b7828
azmal99 <rizkyazmal99@gmail.com>
2022-07-13 23:14:14 +0700

    use

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -17,12 +17,12 @@ use App\Models\AnggotaEkstrakulikuler; //File Model
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Arr;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Carbon\Carbon;
-
 class SiswaController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
