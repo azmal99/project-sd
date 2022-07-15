77d2719e43df8fc23ef7763051bf0656a257205e
azmal99 <rizkyazmal99@gmail.com>
2022-07-16 03:15:01 +0700

    mantap

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -53,10 +53,9 @@ class KelasController extends BaseController
                         ->select('kd_kelas')
                         ->where('id', '=', $kelas_id)->first();
         $kd_kelas = $kelas_siswa->kd_kelas;
-        $mapel_siswa = DB::table('guru')
+        $mapel_siswa = DB::table('mata_pelajaran')
                         ->select('mata_pelajaran.id', 'mata_pelajaran.kd_mata_pelajaran', 'mata_pelajaran.nama_mata_pelajaran')
                         ->where('mata_pelajaran.kd_mata_pelajaran', 'like', $kd_kelas.'%')
-                        ->join('mata_pelajaran','guru.mata_pelajaran_id','=','mata_pelajaran.id')
                         ->get();
 
         return response()->json([
