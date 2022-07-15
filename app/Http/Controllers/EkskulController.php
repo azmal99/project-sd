a03e05028f100305d381c2815fa62bc980bce3ec
azmal99 <rizkyazmal99@gmail.com>
2022-07-16 02:48:58 +0700

    commit

--- app/Http/Controllers/EkskulController.php
+++ app/Http/Controllers/EkskulController.php
@@ -33,7 +33,7 @@ class EkskulController extends BaseController
     {
         $ekskul = DB::table('ekskul')
                     // ->select(array('ekskul.id', 'guru.nama_guru', 'guru.id', DB::raw('COUNT(anggota_ekstrakulikuler.siswa_id) as jumlah')))
-                    ->select('ekskul.id as ekskul_id', 'ekskul.nama_ekskul', 'guru.nama_guru', 'guru.id as ekskul_id')
+                    ->select('ekskul.id as ekskul_id', 'ekskul.nama_ekskul', 'guru.nama_guru', 'guru.id as guru_id')
                     ->join('guru', 'ekskul.guru_id', '=', 'guru.id')
                     ->join('anggota_ekstrakulikuler', 'ekskul.id', '=', 'anggota_ekstrakulikuler.ekskul_id')
                     // ->group_by('anggota_ekstrakulikuler.siswa_id')
