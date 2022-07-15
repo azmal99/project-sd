8d1e8e4af521bb3e33ec69a8ec9273eb037de489
azmal99 <rizkyazmal99@gmail.com>
2022-07-16 03:12:41 +0700

    fixing

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -54,8 +54,7 @@ class KelasController extends BaseController
                         ->where('id', '=', $kelas_id)->first();
         $kd_kelas = $kelas_siswa->kd_kelas;
         $mapel_siswa = DB::table('guru')
-                        ->select('mata_pelajaran.kd_mata_pelajaran', 'mata_pelajaran.nama_mata_pelajaran', 'guru.id',
-                                 'guru.nama_guru')
+                        ->select('mata_pelajaran.id', 'mata_pelajaran.kd_mata_pelajaran', 'mata_pelajaran.nama_mata_pelajaran')
                         ->where('mata_pelajaran.kd_mata_pelajaran', 'like', $kd_kelas.'%')
                         ->join('mata_pelajaran','guru.mata_pelajaran_id','=','mata_pelajaran.id')
                         ->get();
