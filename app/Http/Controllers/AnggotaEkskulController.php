9e8110ea86447e92ceaba82476b0bd7e540898d3
azmal99 <rizkyazmal99@gmail.com>
2022-07-17 03:31:19 +0700

    add DB

--- app/Http/Controllers/AnggotaEkskulController.php
+++ app/Http/Controllers/AnggotaEkskulController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\AnggotaEkstrakulikuler; //File Model
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
